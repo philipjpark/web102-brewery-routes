289a3d963ff05c1175688ababa4b119fa86d5e81
philipjpark <philipparknyc@gmail.com>
2023-04-02 18:10:35 -0400

    search bar filters by state

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css';
 
 function App() {
     const [list, setList] = useState(null);
+    //search bar for states 
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
       const fetchAllBreweries = async () => {
@@ -17,15 +19,33 @@ function App() {
       fetchAllBreweries().catch(console.error);
     }, []);
     
+    const handleInputChange = event => {
+      setQuery(event.target.value);
+  };
 
   return (
     <div className="whole-page"> 
       <h1>🍺Breweries List</h1>
       <h3>Search for breweries.</h3>
 
-      {list && Object.entries(list).map(([key, value]) => (
-        <li key={value.id}>{value.name}</li>
-      ))}
+      {/* {list && Object.entries(list).map(([key, value]) => (
+        <li key={value.id}>{value.name}</li> */}
+
+      <input 
+        type="text" 
+        value={query} 
+        onChange={handleInputChange} 
+        placeholder="Search by state" />
+
+<ul>
+  {list && Object.entries(list).map(([key, value]) => {
+    if (value.state.toLowerCase().includes(query.toLowerCase())) {
+      return <li key={value.id}>{value.name}</li>
+    }
+    return null;
+  })}
+</ul>
+
 
     </div>
   );
