e7b230dd01a08935d360f885d6d841f81d0313f2
philipjpark <philipparknyc@gmail.com>
2023-04-02 22:36:41 -0400

    established routes and added a home button

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from './routes/Layout';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index={true} element={<App />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+
   </React.StrictMode>,
 )
